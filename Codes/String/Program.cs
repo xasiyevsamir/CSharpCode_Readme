e6ead066d64f0a5441745874fb8b89589a58f139
xasiyevsamir <samirakh@code.edu.az>
2025-09-10 00:50:16 +0400

    update

---
+++ Codes/String/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Array array= Array.CreateInstance(typeof(int),5);
+
+            foreach(var i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            Type intArrayType = typeof(int[]);
+            Console.WriteLine($"{intArrayType.Name} : {intArrayType.BaseType?.Name}");
+
+            Type stringArrayType = typeof(string[]);
+            Console.WriteLine($"{stringArrayType.Name} : {stringArrayType.BaseType?.Name}");
+
+            string word = "Hello World";            
+            string data= "Hello WorlD";
+            string data1= "Hello WorlD";
+            Console.WriteLine(data.Equals(data1, StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(string.Equals(data, data1, StringComparison.OrdinalIgnoreCase));
+
+            int a = 0;
+            a.CompareTo(word);
+            Console.WriteLine(a);
+            int[] ints = { 1, 2, 3 };
+
+           
+            Console.WriteLine(ints.Length);
+            Console.WriteLine(ints.Rank);
+            Console.WriteLine(ints[1]);
+        }       
+    }
+}
---
+++ Codes/ValueType/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ValueType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "123.45";
+            // string tipli dəyişən yaradılır, dəyəri "123.45"
+
+            decimal number = decimal.Parse(str);
+            // decimal.Parse → string-i decimal tipinə çevirir
+            // str = "123.45" → number = 123.45
+
+            decimal number2;
+            // Yeni decimal dəyişən elan edilir
+
+            bool success = decimal.TryParse("200.5", out number2);
+            // decimal.TryParse → string-i decimal tipinə çevirməyə çalışır
+            // Əgər uğurlu olarsa, number2 = 200.5 və success = true olur
+            // Əgər uğursuz olarsa, success = false və number2 default dəyər alır (0.0)
+
+            Console.WriteLine(number2);
+            // Çap edir: 200.5
+
+            Console.WriteLine(success);
+            // Çap edir: True
+
+
+            //string str = "123.45";
+            //// string tipli dəyişən yaradılır, dəyəri "123.45"
+
+            //double number = double.Parse(str);
+
+            //string str = "123";
+            //// string tipli dəyişən yaradılır, dəyəri "123"
+
+            //int number = int.Parse(str);
+            //// int.Parse → string-i int tipinə çevirir
+            //// str = "123" → number = 123
+
+
+            //string str = "123";
+            //// string tipli dəyişən yaradılır, dəyəri "123"
+
+            //byte number = byte.Parse(str);
+        }
+    }
+}
